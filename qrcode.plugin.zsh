ddd4170efb983dc80dd33ca6d9110eff04f7ebec
liuzhuoming <416561115@qq.com>
2022-02-15 13:45:02 +0800

    update `sh` to `bash`

--- qrcode.plugin.zsh
+++ qrcode.plugin.zsh
@@ -1,1 +1,1 @@
-alias qrcode="cd `dirname $0` && sh qrcode.sh"
+alias qrcode="cd `dirname $0` && bash qrcode.sh"
--- qrcode.sh
+++ qrcode.sh
@@ -5,10 +5,10 @@ if [ $# == 0 ]; then
 else
     if [ $1 == "encode" ]; then
         cd `dirname $0`
-        sh ./encode-qrcode.sh "$2"
+        bash ./encode-qrcode.sh "$2"
     elif [ $1 == "decode" ]; then
         cd `dirname $0`
-        sh ./decode-qrcode.sh "$2"
+        bash ./decode-qrcode.sh "$2"
     else
         echo "error: param[$1] is not supported"
     fi
