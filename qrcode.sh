726f194c6b0bb9c5e186d6438a2a6960fd2e6257
liuzhuoming <416561115@qq.com>
2022-02-16 18:55:21 +0800

    cd `pwd` when script finished

--- qrcode.sh
+++ qrcode.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+PWD=`pwd`
+
 if [ $# == 0 ]; then
     echo "error: param[1] is required"
 else
+    cd `dirname $0`
     if [ $1 == "encode" ] || [ $1 == "-e" ]; then
-        cd `dirname $0`
         bash ./qrcode-encode.sh "$2"
     elif [ $1 == "decode" ] || [ $1 == "-d" ]; then
-        cd `dirname $0`
         bash ./qrcode-decode.sh "$2"
     else
         echo "error: param[$1] is not supported"
     fi
+    cd $PWD
 fi
