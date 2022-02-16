3319f119892fc5ad0877154bade5cf27910a56b1
liuzhuoming <416561115@qq.com>
2022-02-16 17:52:56 +0800

    add -param

--- qrcode.sh
+++ qrcode.sh
@@ -3,10 +3,10 @@
 if [ $# == 0 ]; then
     echo "error: param[1] is required"
 else
-    if [ $1 == "encode" ]; then
+    if [ $1 == "encode" || $1 == "-e" ]; then
         cd `dirname $0`
         bash ./qrcode-${1}.sh "$2"
-    elif [ $1 == "decode" ]; then
+    elif [ $1 == "decode" || [ $1 == "-d" ]]; then
         cd `dirname $0`
         bash ./qrcode-${1}.sh "$2"
     else
