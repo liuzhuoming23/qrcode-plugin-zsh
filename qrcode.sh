02dbe36c0ee79fe0d5ac901b3e93c8964802acf2
liuzhuoming <416561115@qq.com>
2022-02-16 20:27:51 +0800

    fix .sh path

--- qrcode.sh
+++ qrcode.sh
@@ -7,9 +7,9 @@ if [ $# == 0 ]; then
 else
     cd `dirname $0`
     if [ $1 == "--encode" ] || [ $1 == "-e" ]; then
-        bash ./qrcode-encode.sh "$2"
+        bash ./encode.sh "$2"
     elif [ $1 == "--decode" ] || [ $1 == "-d" ]; then
-        bash ./qrcode-decode.sh "$2"
+        bash ./decode.sh "$2"
     else
         echo "error: param[$1] is not supported"
     fi
